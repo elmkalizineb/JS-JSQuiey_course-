c1c75285b4fc50cf8a327585adc384dc4b81dad4
elmkalizineb <elmkalizineb@gmail.com>
2025-06-29 19:28:57 +0100

    DOM-Navigation

---
+++ Navigation.js
@@ -0,0 +1,12 @@
+const container=document.getElementById("container");
+console.log("container element ",container);
+
+const listItems = document.getElementsByTagName("li");
+console.log("list items : ",listItems);
+
+
+const contentDiv=document.getElementsByClassName("container");
+console.log("content divs ", contentDiv);
+
+const welcomeP=document.querySelector("#container .content p" );
+console.log("welcome paragraph",welcomeP);
