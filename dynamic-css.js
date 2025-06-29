ddc94bfb4dfeead4683deb5ef34cc19ab6839327
elmkalizineb <elmkalizineb@gmail.com>
2025-06-29 13:20:50 +0100

    DOM-Css

---
+++ dynamic-css.js
@@ -0,0 +1,23 @@
+const dynamicElement =  document.getElementById('dynamicElement');
+
+dynamicElement.style.background ='lightblue';
+dynamicElemengeColor
+
+function changeColor() {
+
+    const randonColor=getRandomColor();
+
+    dynamicElement.style.backgroundColor = randonColor;
+   
+}
+
+function  getRandomColor() {
+    const letters ="0123456789ABCDEF";
+    let color ="#";
+    for (let i= 0; i < 6; i++) {
+        
+        color+=letters[Math.floor(Math.random() * 16)];
+        
+    }
+    return color;
+}
