7f0f350b0198ba389fabca7a6382f0f1cf3c89ca
elmkalizineb <elmkalizineb@gmail.com>
2025-06-30 10:26:02 +0100

    Function Cluster

---
+++ FunctionClosures.js
@@ -0,0 +1,32 @@
+
+//  basic closure 
+function outer() {
+  let message = "Hello from outer!";
+
+  function inner() {
+    console.log(message); // Still has access to outer's variable
+  }
+
+  return inner;
+}
+
+const greet = outer();  // outer() runs and returns inner()
+greet();  // Hello from outer!
+ 
+// count example 
+function createCounter(){
+    let count = 0; // outer variable 
+
+    return function(){
+        count++; console.log("Count : ",count);
+    }
+}
+// each counter has its own private count variable 
+const couter1 =createCounter();
+couter1();  // Count : 1 
+couter1(); // Count : 2
+
+const couter2 =createCounter();
+couter2(); // Count : 1
+
+
